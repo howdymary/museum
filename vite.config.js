4f0798e09e0b54c2c3089f726428131afeac2711
howdymary <35550889+howdymary@users.noreply.github.com>
2026-03-22 14:55:24 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('three')) return 'three';
+          if (id.includes('node_modules')) return 'vendor';
+        }
+      }
+    }
+  }
+})
